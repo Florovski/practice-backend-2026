50be218cb0ab312c97ba164fc6eef1272bd89d54
Maksim Kreymer <saks8797@mail.ru>
2026-03-02 19:54:44 +0700

    add PostmanCollections

--- src/database/migrations/2026_03_01_080300_create_reviews_table.php
+++ src/database/migrations/2026_03_01_080300_create_reviews_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('booking_id')->constrained()->onDelete('cascade');
             $table->foreignId('resource_id')->constrained()->onDelete('cascade');
-            $table->integer('rating'); // от 1 до 5
+            $table->integer('rating');
             $table->text('comment')->nullable();
             $table->timestamps();
         });
